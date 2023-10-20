d79e6c70819140918af2d12f8c203685584017f9
zdana <zdana@ucsc.edu>
2023-10-20 13:01:49 -0700

    Code prompt 1

---
+++ name.py
@@ -0,0 +1,12 @@
+def print_info():
+    name = "Zachary Dana"
+    pronouns = "He/Him"
+    fav_movie = "Goodfellas"
+    fav_food = "Indian food"
+
+    print(f"My name is {name} and my preferred pronouns are {pronouns}. My favorite movie is Martin Scorsese's \
+{fav_movie} because of its action scenes captivating story. My favorite food is {fav_food} because of \
+its intense and exotic flavors.")
+
+# Call the function
+print_info()
